c40ab393fb75377aa94fa9abd25837c6ee21f09d
ludozing <ludozing@gmail.com>
2022-02-08 10:17:21 +0900

    case가 아니라 event

--- src/timeline/TimelineList.js
+++ src/timeline/TimelineList.js
@@ -3,35 +3,30 @@ import React, { useEffect, useState } from 'react';
 import useAsync from '../hooks/useAsync';
 import './TimelineList.scss'
 
-async function getCases(){
+async function getEvents(){
     const response = await axios.get(
-        'http://localhost:8080/cases'
+        'http://localhost:8080/events'
     )
     return response.data;
 }
-function dateCoordination(d){
-    return (
-        new Date(d.getTime() - (d.getTimezoneOffset() * 60000)).toISOString()
-    )
-}
 
 function TimelineList(props) {
-    const state = useAsync(getCases);
-    const {loading, error, data: cases} = state;
+    const state = useAsync(getEvents);
+    const {loading, error, data: events} = state;
     if(loading) return <div>로딩중...</div>
     if(error) return <div>페이지를 나타낼 수 없습니다.</div>
-    if(!cases) return null;
+    if(!events) return null;
     return (
         <div className='timeline innerCon'>
             <h2>TIMELINE</h2>
             <ul>
-                {cases.map(data => {
+                {events.map(data => {
                     console.log(typeof data.date)
-                    let c_date = new Date(+new Date(data.date) + 3240 * 10000).toISOString().split("T")[0];
+                    let e_date = new Date(+new Date(data.date) + 3240 * 10000).toISOString().split("T")[0];
                     return(
                         <li key={data.no} className='timelineLi'>
-                            <p className='c_date'>{c_date}</p>
-                            <p className='c_title'>{data.title}</p>
+                            <p className='e_date'>{e_date}</p>
+                            <p className='e_title'>{data.title}</p>
                         </li>
                     )
                 })}
