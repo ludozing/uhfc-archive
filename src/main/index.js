f443d57df2e08ce5a1b99e740d232d9866b45638
ludozing <ludozing@gmail.com>
2022-02-08 17:50:43 +0900

    쿼리셀렉터 대신 useRef로 고쳐야 함

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useCallback, useLayoutEffect } from 'react';
 import './MainPage.scss'
 
 function MainPage() {
+    const [ currentLoopIndex, setCurrentLoopIndex ] = useState(0);
+    let lastImgDiv = document.querySelector(".bgImg05");
+    let mainPage = document.querySelector("#mainPage");
+    console.log(mainPage)
+    mainPage.appendChild(lastImgDiv);
+    useLayoutEffect(()=>{
+        if(currentLoopIndex<5){
+            setInterval(()=>{
+                setCurrentLoopIndex(currentLoopIndex +1 );
+            }, 3000)
+        }else if(currentLoopIndex=5){
+            setCurrentLoopIndex(0);
+        }
+        console.log(currentLoopIndex)
+    })
+
     return (
         <div id="mainPageFrame">
-            <div id="mainPage">
+            <div id="mainPage" style={`left:calc(${currentLoopIndex} * 100%)`}>
                 <div className='mainBg bgImg01' />
                 <div className='mainBg bgImg02' />
                 <div className='mainBg bgImg03' />
